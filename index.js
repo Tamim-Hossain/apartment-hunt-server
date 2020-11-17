ffceab0e2838e08a6af02c3e7fe4db499d486c89
Tamim <tamim-hossain>
2020-11-17 13:14:08 +0600

    bug fixed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+const cors = require('cors');
+const port = 4000;
 const ObjectId = require('mongodb').ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q43xx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
