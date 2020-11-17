61098d4e3625ac7cde69404793f05bc684d8dca7
Rayhan0Islam0Shagor <67514865+Rayhan0Islam0Shagor@users.noreply.github.com>
2020-11-17 12:38:04 +0600

    try it. onek moja

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const app = express();
-const port = 4000;
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const ObjectId = require('mongodb').ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q43xx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-app.use(bodyParser.json());
+const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hello From db, it's working")
-})
+app.use(bodyParser.json());
+const port = 4000;
+app.use(fileUpload());
 
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+app.get('/', (req, res) => {
+    res.send("Server is Ready mama!")
+})
+
 
 client.connect((err) => {
     const bookingsCollection = client.db('apartmentHunt').collection('bookings');
@@ -41,8 +46,16 @@ client.connect((err) => {
         });
     });
 
+    // for use Params try to solve it
+    app.get('/bookings/:id', (req, res) => {
+        const intId = parseInt(req.params.id)
+        eventCollection.find({ id: intId })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
 
-
+    // To get RentsData
     app.get('/rents', (req, res) => {
         rentsCollection.find({}).toArray((err, documents) => {
             res.send(documents);
@@ -56,6 +69,7 @@ client.connect((err) => {
         });
     });
 
+
     app.get('/add-rent', (req, res) => {
         addRentsCollection.find({}).toArray((err, documents) => {
             res.send(documents);
@@ -68,6 +82,31 @@ client.connect((err) => {
             res.send(result.insertedCount > 0);
         });
     });
+
+    // add Rent House done
+    app.post('/addRent', (req, res) => {
+        const name = req.body.name;
+        const email = req.body.email;
+        const title = req.body.title;
+        const location = req.body.location;
+        const bath = req.body.bath;
+        const price = req.body.price;
+        const bed = req.body.bed;
+        const file = req.files.image;
+        const Img = file.data;
+        const encImg = Img.toString('base64');
+        const image = {
+            contentType: req.files.image.mimetype,
+            size: req.files.image.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        addRentsCollection.insertOne({ name, email, title, location, bath, price, bed, image })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
 });
 
 app.listen(process.env.PORT || port);
